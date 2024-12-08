7317f3fd5308f001f794bf85b0d0b4fb62c5b9e5
Nanuzer0 <ivragov3@gmail.com>
2024-12-08 17:09:38 +0300

    add 6 cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
+function createCard(cardData) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  // Добавляем обработчик для удаления карточки
+  deleteButton.addEventListener('click', () => {
+    cardElement.remove();
+  });
+
+  return cardElement;
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData);
+  cardsList.append(cardElement);
+});
